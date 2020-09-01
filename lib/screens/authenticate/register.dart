180b5e63549825c1133bbe6bedbd933237a7be4a
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-01 15:53:37 +0530

    text decoration for input box in register form.

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -40,6 +40,17 @@ class _RegisterState extends State<Register> {
             children: <Widget>[
               SizedBox(height: 20.0,),
               TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white,width: 2.0)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown[400],width: 2.0)
+                  )
+                  ),
                 validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val){
                   setState(() => email = val);
