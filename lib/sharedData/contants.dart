84448aa5a338505f512106e2b9396f1afbb3ccae
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-01 16:05:41 +0530

    remove hinttext fro constant data.

--- lib/sharedData/contants.dart
+++ lib/sharedData/contants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 const textInputDecoration = InputDecoration(
-    hintText: 'Email',
     fillColor: Colors.white,
     filled: true,
     enabledBorder: OutlineInputBorder(
