5bc80e691cdf1a522ffc78e5b3da8ba05b2095af
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-03 09:14:38 +0530

    add an FormTextInput filed 'name' .

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -16,6 +16,7 @@ class _RegisterState extends State<Register> {
   String email = '';
   String password = '';
   String error = '';
+  String name = '';
   bool loading = false;
   @override
   Widget build(BuildContext context) {
@@ -35,58 +36,70 @@ class _RegisterState extends State<Register> {
         ],
 
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0,),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val){
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0,),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                obscureText: true,
-                validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                onChanged: (val){
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0,),
-              RaisedButton(
-                onPressed: () async {
-                  if(_formkey.currentState.validate())
-                    {
-                      setState(() => loading = true );
-                      dynamic result = await _auth.registerEmailandPassword(email,password);
-                      if(result == null)
-                        {
-                          setState(() {
-                            error = '*Please supply a valid email.';
-                            loading = false;
-                          });
-                        }
-                    }
-                },
-                color: Colors.brown[400],
-                child: Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0,),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Name'),
+                  validator: (val) => val.isEmpty ? 'Enter your name ' : null,
+                  onChanged: (val){
+                    setState(() => name = val);
+                  },
+                ),
+                SizedBox(height: 20.0,),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val){
+                    setState(() => email = val);
+                  },
+                ),
+                SizedBox(height: 20.0,),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                  obscureText: true,
+                  validator: (val) => val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                  onChanged: (val){
+                    setState(() => password = val);
+                  },
+                ),
+                SizedBox(height: 20.0,),
+
+                RaisedButton(
+
+                  onPressed: () async {
+                    if(_formkey.currentState.validate())
+                      {
+                        setState(() => loading = true );
+                        dynamic result = await _auth.registerEmailandPassword(email,password,name);
+                        if(result == null)
+                          {
+                            setState(() {
+                              error = '*Please supply a valid email.';
+                              loading = false;
+                            });
+                          }
+                      }
+                  },
+                  color: Colors.brown[400],
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red,fontSize: 14.0),
-              ),
-            ],
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red,fontSize: 14.0),
+                ),
+              ],
+            ),
           ),
         ),
       ),
