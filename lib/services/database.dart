f29c96935354400f284d86d2a8c6d6e64f825862
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-03 08:34:44 +0530

    define updateUserData at time to new register

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DataBase {
+
+  String uid;
+  DataBase({ this.uid });
+
   final _customer = Firestore.instance.collection('customers');
+
+  Future updateUserData (String sugars , String name , int strength) async {
+    return await _customer.document(uid).setData({
+      'sugars' : sugars,
+      'name' : name,
+      'strength' :strength
+    });
+  }
 }
