a3e5ae85a1f86c55e52eb404bf8264c3645eea82
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-05 12:43:28 +0530

    generate customerTile for each customer

--- lib/screens/home/customerlist.dart
+++ lib/screens/home/customerlist.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_team/models/customer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:coffee_team/screens/home/customertile.dart';
 
 class CustomerList extends StatefulWidget {
   @override
@@ -12,11 +13,11 @@ class _CustomerListState extends State<CustomerList> {
   @override
   Widget build(BuildContext context) {
     final customers = Provider.of<List<Customer>>(context);
-    customers.forEach((Customer) {
-      print(Customer.name);
-      print(Customer.sugars);
-      print(Customer.strength);
-    });
-    return Container();
+    return ListView.builder(
+        itemCount: customers.length,
+        itemBuilder: (context,index){
+          return CustomerTile(customer : customers[index]);
+        }
+    );
   }
 }
