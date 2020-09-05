de723c8b5fe6d04b91e1fefdc32d3893829d4fb0
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-05 11:47:14 +0530

    excess customer list via Customer Model

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,15 +1,15 @@
+import 'package:coffee_team/models/customer.dart';
 import 'package:coffee_team/services/auth.dart';
 import 'package:coffee_team/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coffee_team/screens/home/customerlist.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<QuerySnapshot>.value(
+    return StreamProvider<List<Customer>>.value(
       value: DataBase().customers,
       child: Scaffold(
         backgroundColor: Colors.brown[100],
