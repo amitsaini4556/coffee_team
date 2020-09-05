9b31e8ac4ccb8ea1693c1b32c62c2a6386e69c9e
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-05 12:43:56 +0530

    define customerTile

---
+++ lib/screens/home/customertile.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:coffee_team/models/customer.dart';
+
+class CustomerTile extends StatelessWidget {
+  final Customer customer ;
+  CustomerTile({ this.customer});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.brown[customer.strength],
+          ),
+          title: Text(customer.name),
+          subtitle: Text('Takes ${customer.sugars} sugar(s) '),
+        ),
+      ),
+    );
+  }
+}
