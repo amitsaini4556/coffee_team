ef3c8a9f4dce388518cf162e118cc5b8fcf60473
amitsaini4556 <amitsaini2701@gmail.com>
2020-09-10 22:06:07 +0530

    define userData model for users details like uid,name,sugars,strength

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,3 +2,11 @@ class User {
   final String uid;
   User ( { this.uid });
 }
+
+class UserData {
+  String uid;
+  String name;
+  String sugars;
+  int strength;
+  UserData({this.uid,this.name,this.sugars,this.strength});
+}
